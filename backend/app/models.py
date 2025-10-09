13ba22d5fa7e406dc0aa9f45971943753455958a
Alexander Sarver <ajsarver87@gmail.com>
2025-10-08 22:04:30 -0500

    add initial user model

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,1 +1,25 @@
+from database import Base
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from datetime import datetime
+
+
 ## create the tables here, need to figure out starting data models
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    is_admin = Column(Boolean, default=False)
+    is_superuser = Column(Boolean, default=False)
+    created_at = Column(DateTime, default=datetime.now(datetime.UTC))
+    updated_at = Column(
+        DateTime,
+        default=datetime.now(datetime.UTC),
+        onupdate=datetime.now(datetime.UTC),
+    )
+    last_login = Column(DateTime, default=None, nullable=True)
