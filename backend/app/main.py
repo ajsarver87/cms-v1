998b7c5396e3331e403324e756c74ba5d487c715
Alexander Sarver <ajsarver87@gmail.com>
2025-10-10 13:30:49 -0500

    get user registration and login working

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from typing import Annotated
 from . import models
 from .database import engine
 from .routers import auth
@@ -9,7 +10,9 @@ models.Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router)
 
+user_dependency = Annotated[dict, Depends(auth.get_current_user)]
+
 
 @app.get("/")
-async def read_root():
-    return {"message": "Hello World"}
+async def read_root(user: user_dependency):
+    return {"message": f"Hello {user['username']}"}
--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -1,15 +1,23 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
 from ..models import User
-from passlib.context import CryptContext
+from pwdlib import PasswordHash
 from ..database import SessionLocal
 from typing import Annotated
 from sqlalchemy.orm import Session
 from starlette import status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from datetime import timedelta, datetime, timezone
+import jwt
 
+router = APIRouter(prefix="/auth", tags=["Auth"])
 
-router = APIRouter()
-bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+SECRET_KEY = "dee70bf00d73d99c71d5e9a09f1a9baf928001c70655c2e346fa6ac24672a691"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+password_hash = PasswordHash.recommended()
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="auth/token")
 
 
 class createUserRequest(BaseModel):
@@ -20,6 +28,11 @@ class createUserRequest(BaseModel):
     password: str
     is_admin: bool = False
     is_superuser: bool = False
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
 
 
 def get_db():
@@ -33,16 +46,57 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
+def verify_password(plain_password, hashed_password):
+    return password_hash.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return password_hash.hash(password)
+
+
+def authenticate_user(username: str, password: str, db: db_dependency):
+    user = db.query(User).filter(User.username == username).first()
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+def create_access_token(username: str, userid: int, expires_delta: timedelta):
+    encode = {"sub": username, "id": userid}
+    expires = datetime.now(timezone.utc) + expires_delta
+    encode.update({"exp": expires})
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        user_id: int = payload.get("id")
+        if username is None or user_id is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid Authentication Token",
+            )
+        return {"username": username, "id": user_id}
+    except jwt.PyJWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid Authentication Token",
+        )
+
+
 # Register a New User
-# TODO: Fix Password Hashing functionality
-@router.post("/auth/register", status_code=status.HTTP_201_CREATED)
+@router.post("/register", status_code=status.HTTP_201_CREATED)
 async def register_user(db: db_dependency, create_user_request: createUserRequest):
     create_user_model = User(
         email=create_user_request.email,
         username=create_user_request.username,
         first_name=create_user_request.first_name,
         last_name=create_user_request.last_name,
-        hashed_password=bcrypt_context.hash(create_user_request.password),
+        hashed_password=get_password_hash(create_user_request.password),
         is_admin=create_user_request.is_admin,
         is_superuser=create_user_request.is_superuser,
     )
@@ -54,8 +108,25 @@ async def register_user(db: db_dependency, create_user_request: createUserReques
 
 
 # Login a User
+@router.post("/token", response_model=Token)
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency
+):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+        )
+    token = create_access_token(
+        user.username, user.id, timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    )
+    return {"access_token": token, "token_type": "bearer"}
+
 
 # Logout a User
+
+# Get a Token
 
 # Refresh Token
 
@@ -66,5 +137,4 @@ async def register_user(db: db_dependency, create_user_request: createUserReques
 # Verify Email
 
 # Provider Login (OAuth2)
-
 # Provider Callback (OAuth2)
